6bfefc69ef9d638f9715bd62ac653466bf7cda0a
Sunil <sunil@Spartacus.domain>
2021-08-02 22:22:55 -0600

    initial commit

---
+++ test1.py
@@ -0,0 +1,14 @@
+import os
+#import numpy as np
+
+a = 1
+print(f'starting test {a}...')
+
+def f1():
+    firstList = [1,2,3,4]
+    secondList = [4,5,6,7]
+#   arr1 = np.array()
+    print(f'inside f1..')
+
+if __name__ == '__main__':
+    f1()
